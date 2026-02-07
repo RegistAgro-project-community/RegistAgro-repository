8574b59558f306052e68d9dbe55ae1ec903a5b1c
eucleniocadete <eucleniocadete@gmail.com>
2026-02-07 22:28:11 +0100

    Add Integration Login function

--- lib/view/login-signup/loginScreen/login.dart
+++ lib/view/login-signup/loginScreen/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:projecto_registagro/repositories/auth/login.dart';
 import 'package:projecto_registagro/view/login-signup/homeScreen/homescreen.dart';
 import 'package:projecto_registagro/view/login-signup/signUp/signup.dart';
 import 'package:projecto_registagro/shared/arraow_back/arrow_back.dart';
@@ -8,7 +9,7 @@ import 'package:projecto_registagro/shared/buttom_logoText/button_submit.dart';
 import 'package:projecto_registagro/shared/formInput/input.dart';
 import 'package:projecto_registagro/shared/imageLogo/imagelogo.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:projecto_registagro/view/pages/main_page/main_page.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -22,21 +23,20 @@ class _LoginState extends State<Login> {
   final _emailCtrl = TextEditingController();
   final _passwordCtrl = TextEditingController();
   bool isObscure = true;
-  
 
-    final List<Map<String, String>> users = [
-      {"email": "elias.matingo@gmail.com", "password": "@Anselmo"},
-      {"email": "claudio.cassoma@gmail.com", "password": "@cassoma777"},
-      {"email": "leocania.melo@gmail.com", "password": "@melo123"},
-      {"email": "debora.francisco@gmail.com", "password": "@francisco90"},
-      {"email": "ildeberto.vasconcelos@gmail.com", "password": "@vasconcelos"},
-    ];
+  final List<Map<String, String>> users = [
+    {"email": "elias.matingo@gmail.com", "password": "@Anselmo"},
+    {"email": "claudio.cassoma@gmail.com", "password": "@cassoma777"},
+    {"email": "leocania.melo@gmail.com", "password": "@melo123"},
+    {"email": "debora.francisco@gmail.com", "password": "@francisco90"},
+    {"email": "ildeberto.vasconcelos@gmail.com", "password": "@vasconcelos"},
+  ];
 
-    void _toggleObscure(){
-      setState(() {
-        isObscure = !isObscure;
-      });
-    }
+  void _toggleObscure() {
+    setState(() {
+      isObscure = !isObscure;
+    });
+  }
 
   @override
   void dispose() {
@@ -50,9 +50,7 @@ class _LoginState extends State<Login> {
       return 'Informe o email';
     }
 
-    final emailRegex = RegExp(
-      r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-    );
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
 
     if (!emailRegex.hasMatch(value)) {
       return 'Email inválido';
@@ -60,6 +58,7 @@ class _LoginState extends State<Login> {
 
     return null;
   }
+
   String? validatePassword(String? value) {
     if (value == null || value.isEmpty) {
       return 'Informe a senha';
@@ -72,171 +71,187 @@ class _LoginState extends State<Login> {
     return null;
   }
 
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-     resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: ArrowBack(
-          onPressed: ()  {
+          onPressed: () {
             Navigator.pushReplacement(
-              context, 
+              context,
               PageTransition(
                 type: PageTransitionType.leftToRight,
                 child: Homescreen(),
-                duration: Duration(milliseconds: 350)
-              )
+                duration: Duration(milliseconds: 350),
+              ),
             );
           },
         ),
       ),
       body: KeyboardVisibilityBuilder(
-       builder: (context, isKeyboardVisible){
-        return AnimatedContainer(
-          duration: Duration(milliseconds: 250),
-          padding: EdgeInsets.only(
-            bottom: isKeyboardVisible ? 20.h : 0.h,
-          ),
-          child: SingleChildScrollView(
-            child: Center(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-                child: Column(
-                  children: [
-                    Imagelogo(
-                      height: 150.h,
-                    ),
-                    SizedBox(height: 24.h),
-                    Text(
-                      'Bem-vindo!',
-                      style: TextStyle(fontSize: 28.sp, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 8.h),
-                    Text(
-                      'Faça login na sua conta \ne agroconecta-te',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16.sp, color: Colors.black54),
-                    ),
-                    SizedBox(height: 24.h),
-                    Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          Input(
-                            controller: _emailCtrl,
-                            keyboardType: TextInputType.emailAddress,
-                            isObscure: false,
-                            placeholder: "Insira o seu email",
-                            labelText: "Email",
-                            icon: Icons.alternate_email,
-                            onChanged: (value) {
-                              _emailCtrl.value =  TextEditingValue(
-                                text: value.toLowerCase(),
-                                selection: TextSelection.collapsed(offset: value.length)
-                              );
-                            },
-                            validator: validateEmail,
-                          ),
-                          SizedBox(height: 12.h),
-                          Input(
-                            controller: _passwordCtrl,
-                            keyboardType: TextInputType.visiblePassword,
-                            isObscure: isObscure,
-                            placeholder: "Insira a sua senha",
-                            labelText: "Senha",
-                            sufixIcon: IconButton(
-                              icon: Icon(
-                                isObscure ? Icons.visibility : Icons.visibility_off,
-                              ),
-                              onPressed: _toggleObscure,
-                              splashRadius: 20.r,
-                            ),
-                            icon: Icons.lock,
-                            validator: validatePassword,
-                          ),
-                        ],
+        builder: (context, isKeyboardVisible) {
+          return AnimatedContainer(
+            duration: Duration(milliseconds: 250),
+            padding: EdgeInsets.only(bottom: isKeyboardVisible ? 20.h : 0.h),
+            child: SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w),
+                  child: Column(
+                    children: [
+                      Imagelogo(height: 150.h),
+                      SizedBox(height: 24.h),
+                      Text(
+                        'Bem-vindo!',
+                        style: TextStyle(
+                          fontSize: 28.sp,
+                          fontWeight: FontWeight.bold,
                         ),
-                    ),
-                      
-                    SizedBox(height: 16.h),
-                    ButtonSubmit(
-                      borderRadius: BorderRadius.circular(12.r),
-                      padding: EdgeInsets.symmetric(horizontal: 0),
-                      tilte: "Entrar",
-                      onPressed: () {
-                        final form = _formKey.currentState;
-                        if (form == null) return;
-                        if (!form.validate()) {
-                          return;
-                        }
-                        final email = _emailCtrl.text.trim();
-                        final password = _passwordCtrl.text.trim();
-
-                        final validUser = users.any(
-                          (user) =>
-                            user['email'] == email &&
-                            user['password'] == password,
-                        );
-                        if (validUser) {
-                          // Navigator.pushReplacement(
-                          //   context,
-                          //   PageTransition(
-                          //     type: PageTransitionType.fade,
-                          //     child: Homescreen(),
-                          //     duration: const Duration(milliseconds: 300),
-                          //   ),
-                          // );
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Email ou senha inválidos'),
-                              backgroundColor: Colors.red,
+                      ),
+                      SizedBox(height: 8.h),
+                      Text(
+                        'Faça login na sua conta \ne agroconecta-se',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(height: 24.h),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            Input(
+                              controller: _emailCtrl,
+                              keyboardType: TextInputType.emailAddress,
+                              isObscure: false,
+                              placeholder: "Insira o seu email",
+                              labelText: "Email",
+                              icon: Icons.alternate_email,
+                              onChanged: (value) {
+                                _emailCtrl.value = TextEditingValue(
+                                  text: value.toLowerCase(),
+                                  selection: TextSelection.collapsed(
+                                    offset: value.length,
+                                  ),
+                                );
+                              },
+                              validator: validateEmail,
                             ),
-                          );
-                        }
-                      },
-                    ),
+                            SizedBox(height: 12.h),
+                            Input(
+                              controller: _passwordCtrl,
+                              keyboardType: TextInputType.visiblePassword,
+                              isObscure: isObscure,
+                              placeholder: "Insira a sua senha",
+                              labelText: "Senha",
+                              sufixIcon: IconButton(
+                                icon: Icon(
+                                  isObscure
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: _toggleObscure,
+                                splashRadius: 20.r,
+                              ),
+                              icon: Icons.lock,
+                              validator: validatePassword,
+                            ),
+                          ],
+                        ),
+                      ),
 
-                    SizedBox(height: 8.h),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Não tem uma conta?", style: TextStyle(color: Colors.grey, fontSize: 15.sp, fontWeight: FontWeight.w400),),
-                          TextButton(
-                            onPressed: () => {
-                              Navigator.pushReplacement(
+                      SizedBox(height: 16.h),
+                      ButtonSubmit(
+                        borderRadius: BorderRadius.circular(12.r),
+                        padding: EdgeInsets.symmetric(horizontal: 0),
+                        tilte: "Entrar",
+                        onPressed: () async {
+                          final form = _formKey.currentState;
+                          if (form == null) return;
+
+                          if (!form.validate()) {
+                            return;
+                          }
+
+                          final email = _emailCtrl.text.trim();
+                          final password = _passwordCtrl.text.trim();
+
+                          final res = await login(email, password);
+                          
+                          if (res.containsKey('message')) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("${res['message']}"),
+                                backgroundColor: const Color.fromARGB(255, 0, 255, 64),
+                              ),
+                            );
+                             Navigator.pushReplacement(
                                context,
                                PageTransition(
-                                type: PageTransitionType.rightToLeft,
-                                child: Signup(),
-                                duration: Duration(milliseconds: 350)
-                               ) 
-                              )
-                            },
-                            child: Text(
-                              'Criar conta',
+                                 type: PageTransitionType.fade,
+                                 child: MainPage(),
+                                 duration: const Duration(milliseconds: 300),
+                               ),
+                             );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("${res['error']}"),
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          }
+                        },
+                      ),
+
+                      SizedBox(height: 8.h),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Não tem uma conta?",
                               style: TextStyle(
-                                color: Color(0xFF61983D),
+                                color: Colors.grey,
                                 fontSize: 15.sp,
-                                fontWeight: FontWeight.bold
+                                fontWeight: FontWeight.w400,
                               ),
                             ),
-                          ),
-                        ],
+                            TextButton(
+                              onPressed: () => {
+                                Navigator.pushReplacement(
+                                  context,
+                                  PageTransition(
+                                    type: PageTransitionType.rightToLeft,
+                                    child: Signup(),
+                                    duration: Duration(milliseconds: 350),
+                                  ),
+                                ),
+                              },
+                              child: Text(
+                                'Criar conta',
+                                style: TextStyle(
+                                  color: Color(0xFF61983D),
+                                  fontSize: 15.sp,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        );
-       }
+          );
+        },
       ),
-        );
-      }
+    );
+  }
 }
