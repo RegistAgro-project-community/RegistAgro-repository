57cdf7744ded0356f5245d55d782c5509d2b5f12
eucleniocadete <eucleniocadete@gmail.com>
2026-02-15 03:13:57 +0100

    Add ShowDialog

--- lib/repositories/auth/login.dart
+++ lib/repositories/auth/login.dart
@@ -1,17 +1,32 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:projecto_registagro/repositories/storage.dart';
 
 final dio = Dio(BaseOptions(headers: {'Content-Type': 'application/json'}));
 final storeToken = TokenStorage();
 
-Future<Map<String, String>> login(String email, String password) async {
+Future<Map<String, String>> login(
+  BuildContext context,
+  String email,
+  String password,
+) async {
   final data = {'email': email, 'password': password};
+  
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext dialogContext) {
+      return const Center(child: CircularProgressIndicator());
+    },
+  );
 
   try {
     final res = await dio.post(
-      'http://192.168.18.4:5500/auth/login',
+      'https://api-registagro.onrender.com/auth/login',
       data: data,
     );
+
+    if (context.mounted) Navigator.of(context).pop();
 
     final authHeader = res.headers.value('authorization');
     final token = authHeader?.split(" ")[1];
@@ -24,6 +39,8 @@ Future<Map<String, String>> login(String email, String password) async {
 
     return {'error': "Não foi possível fazer login"};
   } on DioException catch (e) {
+    if (context.mounted) Navigator.of(context).pop();
+
     return {"error": e.response?.data!["error"]};
   }
 }
