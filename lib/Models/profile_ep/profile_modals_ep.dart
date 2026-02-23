1d4c57c14ba31b8eda9c764869f52e8d267c9381
eucleniocadete <eucleniocadete@gmail.com>
2026-02-23 02:22:34 +0100

    Change atributes of farms profile

--- lib/Models/profile_ep/profile_modals_ep.dart
+++ lib/Models/profile_ep/profile_modals_ep.dart
@@ -1,13 +1,15 @@
 class ProfileModel {
   final String id;
-  final String image;
-  final String username;
-  final String description;
+  final String profile;
+  final String name;
 
-  ProfileModel({
-    required this.id,
-    required this.image,
-    required this.username,
-    required this.description,
-  });
+  ProfileModel({required this.id, required this.name, required this.profile});
+
+  factory ProfileModel.fromJson(Map<String, dynamic> json) {
+    return ProfileModel(
+      id: json['id'],
+      name: json['name'],
+      profile: json['profile'],
+    );
+  }
 }
