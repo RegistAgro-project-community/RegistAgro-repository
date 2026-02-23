7c88dbb4b410d8c537a1ad462fb721514bbe2293
eucleniocadete <eucleniocadete@gmail.com>
2026-02-23 02:20:53 +0100

    Change atributes of product class

--- lib/Models/product_ep/product_modals_ep.dart
+++ lib/Models/product_ep/product_modals_ep.dart
@@ -2,33 +2,41 @@ import 'package:projecto_registagro/Models/profile_ep/profile_modals_ep.dart';
 
 class Product {
   final String id;
-  final String title;
-  final String price;
-  final String image;
+  final String name;
   final String description;
-  final String subTitle;
-  final String category;
-  final String province;
-  final String quantity;
-  final String stockStatus;
-  final String recommendedTransport;
-  final ProfileModel supplier;
+  final String price;
+  final String type;
+  final String qtd;
+  final String unit;
+  final String transport;
+  final String photo;
+  final ProfileModel farm;
 
   Product({
+    required this.farm,
     required this.id,
-    required this.title,
-    required this.subTitle,
-    required this.price,
-    required this.image,
+    required this.name,
     required this.description,
-    required this.category,
-    required this.province,
-    required this.quantity,
-    required this.recommendedTransport,
-    required this.supplier,
-    String? stockStatus,
-  }) : stockStatus =
-        (stockStatus == null || stockStatus.trim().isEmpty)
-            ? "Desconhecido"
-            : stockStatus;
+    required this.price,
+    required this.type,
+    required this.qtd,
+    required this.unit,
+    required this.transport,
+    required this.photo,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      farm: json['farm'],
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      photo: json['photo'],
+      price: json['price'],
+      type: json['type'],
+      qtd: json['qtd'],
+      unit: json['unit'],
+      transport: json['transport'],
+    );
+  }
 }
