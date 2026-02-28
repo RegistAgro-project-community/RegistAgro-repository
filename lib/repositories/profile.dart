fc9c56461eaa265b023b5564460d6df26ee38564
eucleniocadete <eucleniocadete@gmail.com>
2026-02-28 02:01:45 +0100

    Add user data integration method

--- lib/repositories/profile.dart
+++ lib/repositories/profile.dart
@@ -1,7 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:projecto_registagro/components/TopNotifications/top_notification.dart';
+import 'package:projecto_registagro/repositories/products.dart';
 import 'package:projecto_registagro/repositories/storage.dart';
 import 'package:projecto_registagro/view/auth/loginScreen/login.dart';
+import 'package:projecto_registagro/view/pages/userProfile/userModal/user_modal.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Profile {
@@ -34,12 +37,10 @@ class Profile {
 
       Navigator.pushAndRemoveUntil(
         context,
-        MaterialPageRoute(
-          builder: (context) => Login(),
-        ),
+        MaterialPageRoute(builder: (context) => Login()),
         (route) => false,
       );
-    }else{
+    } else {
       showTopNotification(
         context,
         title: "Error",
@@ -48,6 +49,88 @@ class Profile {
         icon: Icons.error_outline,
       );
     }
+  }
 
+  Future<UserModel> userData(BuildContext context) async {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) => const Center(child: CircularProgressIndicator()),
+    );
+
+    try {
+      final tokenMap = await TokenStorage().readToken();
+
+      if (tokenMap.containsKey("error") || tokenMap["token"] == null) {
+        ProductsRepositories().handleAuthError(
+          context,
+          tokenMap['error'] ?? "Faça login novamente",
+        );
+
+        throw Exception("Error");
+      }
+
+      final dio = Dio(
+        BaseOptions(
+          headers: {
+            "content-type": "application/json",
+            "authorization": "Bearer ${tokenMap["token"]}",
+          },
+        ),
+      );
+
+      final res = await dio.get(
+        "https://api-registagro.onrender.com/users/profile",
+      );
+
+      Navigator.of(context).pop();
+
+      final data = res.data["data"];
+
+      return UserModel(
+        name: data['name'],
+        email: data['email'],
+        phone: data['phone'],
+        bio: "bio",
+        province: data['province'],
+        adress: data['adress'],
+        photoPath: data['profile']
+      );
+    } on DioException catch (e) {
+      Navigator.of(context, rootNavigator: true).pop();
+
+      String message = "Erro ao carregar produtos";
+
+      if (e.response?.statusCode == 401 || e.response?.statusCode == 403) {
+        message = e.response?.data?['error'] ?? 'Sessão expirada';
+
+        ProductsRepositories().handleAuthError(context, message);
+      } else {
+        message =
+            e.response?.data?['error'] ??
+            e.response?.data?['info'] ??
+            e.message ??
+            message;
+
+        showTopNotification(
+          context,
+          title: "Error",
+          description: message,
+          backgroundColor: Colors.red.shade700,
+          icon: Icons.error_outline,
+        );
+      }
+
+      throw Exception(message);
+    } catch (e) {
+      Navigator.of(context).pop();
+
+      ProductsRepositories().handleAuthError(
+        context,
+        "Ocorreu um erro inesperado",
+      );
+
+      throw Exception("Error");
+    }
   }
 }
