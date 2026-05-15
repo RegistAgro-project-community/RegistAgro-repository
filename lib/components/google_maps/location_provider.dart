92e18a7173f5ea661bfd9b40554fe7a284447edb
eucleniocadete <eucleniocadete@gmail.com>
2026-05-15 20:25:07 +0100

    Get carrier coordinates

--- lib/components/google_maps/location_provider.dart
+++ lib/components/google_maps/location_provider.dart
@@ -2,25 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart' as gmaps;
+import 'package:projecto_registagro/repositories/geolocation.dart';
 
 class SourceLocationProvider extends ChangeNotifier {
   gmaps.LatLng? _currentLatLng;
   String _currentAddress = '';
   bool _isLoading = false;
   String? _error;
+  late BuildContext context;
 
   gmaps.LatLng? get currentLatLng => _currentLatLng;
   String get currentAddress => _currentAddress;
   bool get isLoading => _isLoading;
   String? get error => _error;
 
-  Future<void> fetchCurrentLocation() async {
+  Future<void> fetchCurrentLocation(String orderId) async {
     _isLoading = true;
     _error = null;
     notifyListeners();
 
     try {
-      LocationPermission permission = await Geolocator.checkPermission();
+      /*LocationPermission permission = await Geolocator.checkPermission();
       if (permission == LocationPermission.denied) {
         permission = await Geolocator.requestPermission();
       }
@@ -30,16 +32,20 @@ class SourceLocationProvider extends ChangeNotifier {
         _isLoading = false;
         notifyListeners();
         return;
-      }
+      }*/
 
-      final position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,
+      final coordinates = await GeoLocation().carrierCoordinates(
+        orderId,
       );
 
-      _currentLatLng = gmaps.LatLng(position.latitude, position.longitude);
+      /*final position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );*/
+
+      _currentLatLng = gmaps.LatLng(coordinates[0], coordinates[1]);
       _currentAddress = await _getAddress(_currentLatLng!);
     } catch (e) {
-      _error = 'Não foi possível obter localização';
+      _error = e.toString();
     } finally {
       _isLoading = false;
       notifyListeners();
@@ -66,7 +72,8 @@ class SourceLocationProvider extends ChangeNotifier {
       if (p.street?.isNotEmpty == true) partes.add(p.street!);
       if (p.subLocality?.isNotEmpty == true) partes.add(p.subLocality!);
       if (p.locality?.isNotEmpty == true) partes.add(p.locality!);
-      if (p.administrativeArea?.isNotEmpty == true) partes.add(p.administrativeArea!);
+      if (p.administrativeArea?.isNotEmpty == true)
+        partes.add(p.administrativeArea!);
 
       return partes.isNotEmpty ? partes.join(', ') : 'Local desconhecido';
     } catch (_) {
